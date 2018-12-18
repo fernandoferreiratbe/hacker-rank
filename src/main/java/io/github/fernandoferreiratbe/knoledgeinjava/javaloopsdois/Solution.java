9aa273338de6a05e331c7ae242c8b67511567e76
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2018-12-18 08:46:39 -0200

    Fix java challenge solution

--- src/main/java/io/github/fernandoferreiratbe/knoledgeinjava/javaloopsdois/Solution.java
+++ src/main/java/io/github/fernandoferreiratbe/knoledgeinjava/javaloopsdois/Solution.java
@@ -1,20 +1,31 @@
 package io.github.fernandoferreiratbe.knoledgeinjava.javaloopsdois;
 
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 3;
-        int number = 5;
-        // 5 3 5
-        // (a + 2expN * b)
-        for (int i = 1; i <= number; i++) {
-            int sum = 0;
 
-            for(int j = 0; j < i; j++) {
-                double pow = Math.pow(2, j);
-                sum += (int)(a + pow * b);
+        Scanner in = new Scanner(System.in);
+
+        int t = in.nextInt();
+
+        for (int k = 0; k < t; k++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int s = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (i == 0) {
+                    s += (int) (a + ((Math.pow(2, i)) * b));
+                } else {
+                    s += (int) ((Math.pow(2, i)) * b);
+                }
+                System.out.print(s + " ");
             }
-            System.out.print(sum + " ");
+
+            System.out.println("");
         }
     }
 }
