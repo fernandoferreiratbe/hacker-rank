f414931226eb64e6c934fef4338587f71d629a37
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2019-01-24 15:43:42 -0200

    Implement java substring challenge

---
+++ src/main/java/io/github/fernandoferreiratbe/challenges/substring/Solution.java
@@ -0,0 +1,13 @@
+package io.github.fernandoferreiratbe.challenges.substring;
+
+public class Solution {
+    public static void main(String[] args) {
+        String parameter = "Helloworld";
+
+        int start = 3;
+        int end   = 7;
+
+        System.out.println(parameter.substring(start, end));
+
+    }
+}
